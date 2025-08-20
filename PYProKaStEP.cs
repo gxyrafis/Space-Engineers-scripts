3906ddb7d19856f277b8d4089b05ac37c99cc821
George Xyrafis <58188918+gxyrafis@users.noreply.github.com>
2025-08-21 02:04:43 +0300

    Update PYProKaStEP.cs
    Fixed uhorizontal

--- PYProKaStEP.cs
+++ PYProKaStEP.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -95,7 +95,8 @@ namespace IngameScript
                 Vector3D uxuy = refcockpit.GetShipVelocities().LinearVelocity;
                 double utotal = refcockpit.GetShipSpeed();
                 double uvertical = -Vector3D.Dot(uxuy, Vector3D.Normalize(refcockpit.GetNaturalGravity())); //Find DOWNWARDS SPEED based on gravity pull!
-                double uhorizontal = (Math.Sqrt(uxuy.X * uxuy.X + uxuy.Y * uxuy.Y));
+                double uhorizontal = (uxuy - uvertical * Vector3D.Normalize(refcockpit.GetNaturalGravity())).Length();
+                //double uhorizontal = (Math.Sqrt(uxuy.X * uxuy.X + uxuy.Y * uxuy.Y));
 
                 // Transform into local grid (cockpit-relative)
                 MatrixD cockpitMatrix = refcockpit.WorldMatrix;
